9260772ee83423c40b3949b1d4e1e7e2b444ddfc
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-18 23:59:43 -0400

    c preprocessor conditional code inclusion/exclusion

--- cpp/comments.cpp
+++ cpp/comments.cpp
@@ -4,6 +4,17 @@
   Multi line
  */
 
+/*
+  Preprocessor variable-conditional code inclusion/exclusion,
+  E.g. by VAR=1 in a Makefile.
+*/
+
+#ifdef VAR
+  // ...
+#else
+  // ...
+#endif
+
 int main() {
   return 0;
 }
