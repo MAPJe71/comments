5bcf4e79848421ea149fb6f28a381b0e370f1ba4
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-19 00:07:56 -0400

    elisp macro

--- elisp/comments.el
+++ elisp/comments.el
@@ -2,4 +2,11 @@
 
                                         ; Right-aligned comment (Emacs auto indent)
 
+;; Quoted LISP expressions
+(setq f '(1 2 3))
+
+;; LISP Macro
+(defmacro inc (var)
+  `(setq ,var (1+ ,var)))
+
 (message "Bye") ; Trailing comment
