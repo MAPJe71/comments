34d7c2d88a86b53df85d1dfda68692608abe3831
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 23:06:07 -0400

    gitignore Java classes

---
+++ java/Comments.java
@@ -0,0 +1,15 @@
+/**
+   <p>Java doc</p>
+*/
+public final class Comments {
+  /** <p>Utility class</p> */
+  private Comments() {}
+
+  public static void main(final String[] args) {
+    // Single line
+
+    /* Multi
+       Line
+    */
+  }
+}
