8627312319351bcdab01bd9248f509660673eb65
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-07-27 19:38:01 -0500

    main

--- chicken/comments.scm
+++ chicken/comments.scm
@@ -1,6 +1,18 @@
 #!/usr/bin/env csi -ss
 
-;; Single line
+(require-extension srfi-13) ; strings
+(require-extension srfi-1) ; lists
 
-(display "Hello World!\n")
-(exit)
+(define (main args)
+  (display "Hello\n"))
+
+(define (program)
+  (if (string=? (car (argv)) "csi")
+      (let ((s-index (list-index (lambda (x) (string-contains x "-s")) (argv))))
+        (if (number? s-index)
+            (cons 'interpreted (list-ref (argv) (+ 1 s-index)))
+            (cons 'unknown "")))
+      (cons 'compiled (car (argv)))))
+
+(if (equal? (car (program)) 'compiled)
+    (main (cdr (argv))))
