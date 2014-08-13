3c8bdb07a204abfc3d8a289b3bd56ddd74599dcb
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-13 14:54:07 -0500

    javascript

---
+++ javascript/comments.js
@@ -0,0 +1,10 @@
+// Single line
+
+/* Multi-
+line */
+
+var body = document.getElementsByTagName("body")[0];
+var paragraph = document.createElement("p");
+var textNode = document.createTextNode("Bye");
+paragraph.appendChild(textNode);
+body.appendChild(paragraph);
