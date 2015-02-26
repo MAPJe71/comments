f8427a432ec87385f6c7ef7cca909196408263a0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-02-25 21:01:48 -0600

    jslint

--- node/Gruntfile.js
+++ node/Gruntfile.js
@@ -1,9 +1,12 @@
+"use strict";
+
 module.exports = function(grunt) {
   grunt.initConfig({
 		exec: {
 			test: "npm test",
 
       jshint: "node_modules/jshint/bin/jshint .",
+      jslint: "find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name '*[-.]min.js' -prune -o -type f -name '*.js' -exec node_modules/jslint/bin/jslint.js {} \\;",
 		}
   });
 
@@ -11,5 +14,13 @@ module.exports = function(grunt) {
 
   grunt.registerTask("default", ["exec:test"]);
   grunt.registerTask("test", ["exec:test"]);
-  grunt.registerTask("lint", ["exec:jshint"]);
+
+  grunt.registerTask("lint", [
+    "exec:jshint",
+    // Go to bed, Crockford
+    // "exec:jslint",
+  ]);
+
+  grunt.registerTask("jshint", ["exec:jshint"]);
+  grunt.registerTask("jslint", ["exec:jslint"]);
 };
